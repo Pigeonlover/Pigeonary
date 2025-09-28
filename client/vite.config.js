ff9b7c01201391b87a79cc9edae6dded32ae8f97
Pigeonlover <bravefoot@outlook.com>
2025-09-28 18:34:12 +0100

    Navigation edits

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
+  build: { rollupOptions: { input: "index.html" } },
   plugins: [react(), tailwindcss()],
 });
